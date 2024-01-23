69990a216500c8434869acc8933208713a6b52fb
PasqualeIannace <pasquale.iannace@gmail.com>
2024-01-23 19:52:12 +0100

    MILESTONE 2

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Movie;
+use App\Models\Train;
 
 class PageController extends Controller
 {
     public function index()
     {
-        // $trains = Train::all();
+        $trains = Train::all();
+        $inPartenza = Train::where('orario_di_partenza', '>=', now())->get();
 
-        //return view('home', compact("movies"));
+        return view('home', compact("trains", "inPartenza"));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
 
+<h1>Treni in partenza</h1>
+@foreach ($inPartenza as $train)
+<div>
+    <h3>{{$train->azienda}}</h3>
+    <p>da {{$train->stazione_di_partenza}} a {{$train->stazione_di_arrivo}}</p>
+    <p>partenza alle: {{$train->orario_di_partenza}} arrivo alle: {{$train->orario_di_arrivo}}</p>
+    <p>Treno n{{$train->codice_treno}}</p>
+</div>
+@endforeach
 @endsection
